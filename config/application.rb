871dd06bfcb608666cb9aaab319b62da992b6f5f
suzuki_yuuki <suzukiyuki@suzukiyukinoMacBook-puro.local>
2018-07-27 02:45:12 +0900

    bootstrap

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -15,5 +15,6 @@ module Testapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
