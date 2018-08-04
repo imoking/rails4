fe856f3a50d811a21a64535a2c29136d127a2944
suzuki_yuuki <suzukiyuki@suzukiyukinoMacBook-puro.local>
2018-08-05 02:33:41 +0900

    destroy action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,14 +10,10 @@ module ApplicationHelper
 
   def bootstrap_class_for(flash_type)
     case flash_type
-      when :success
-        "alert-success"
-      when :error
-        "alert-danger"
-      when :alert
-        "alert-warning"
-      when :notice
-        "alert-info"
+      when 'alert'
+          "alert-danger"
+      when 'notice'
+          "alert-success"
       else
         flash_type.to_s
     end
